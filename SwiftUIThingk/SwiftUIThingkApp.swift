232043aace21a628ae7a6dc9111df46a22fdab7e
riozron <riozron@icloud.com>
2024-01-06 11:54:33 +0800

    Initial Commit

---
+++ SwiftUIThingk/SwiftUIThingkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThingkApp.swift
+//  SwiftUIThingk
+//
+//  Created by riozron on 2024/1/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThingkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
